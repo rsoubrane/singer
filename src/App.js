ce2d213eb97206d7cb655ad89248e7f090d7746d
rsoubrane <rsoubrane@free-center.fr>
2020-03-03 16:42:00 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Styles
+import "./App.scss";
+
+//Pages
+import Home from "./pages/Home";
+
+function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/' component={Home} />
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
